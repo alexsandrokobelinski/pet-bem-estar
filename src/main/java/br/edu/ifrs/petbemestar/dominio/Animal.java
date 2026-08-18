ec920d4853fefeb6a48b455f9e81be165e6daf2e
alexkobe <alexkobe@gmail.com>
2026-08-18 14:47:57 -0300

    Class Animal

--- src/main/java/br/edu/ifrs/petbemestar/dominio/Animal.java
+++ src/main/java/br/edu/ifrs/petbemestar/dominio/Animal.java
@@ -1,5 +1,53 @@
 package br.edu.ifrs.petbemestar.dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animal {
 
+	private Long id;
+	private String nome;
+	private Dono dono;
+
+	private List<Agendamento> agendamentos = new ArrayList<>();
+
+	public Animal() {
+	}
+
+	public Animal(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Dono getDono() {
+		return dono;
+	}
+
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+
+	public List<Agendamento> getAgendamentos() {
+		return agendamentos;
+	}
+
+	public void adicionarAgendamento(Agendamento agendamento) {
+		agendamentos.add(agendamento);
+		agendamento.setAnimal(this);
+	}
 }
