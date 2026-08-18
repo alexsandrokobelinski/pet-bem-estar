1b37812557c6ef6340f697a85532133fdabec102
alexkobe <alexkobe@gmail.com>
2026-08-18 15:26:56 -0300

    cria Principal

---
+++ src/main/java/br/edu/ifrs/petbemestar/Principal.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.petbemestar;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println("Pet Bem-Estar");
+
+	}
+
+}
